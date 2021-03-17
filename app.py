ef4fd969f4a56278bf1039089dfcc1a6488ca8f0
David-Gyavi <dbloosi12@gmail.com>
2021-03-17 00:00:49 +0000

    Add Register template

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -24,6 +25,27 @@ mongo = PyMongo(app)
 def get_fields():
     fields = mongo.db.fields.find()
     return render_template("fields.html", fields=fields)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # Checking if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+
+        if existing_user:
+            flash("Username already exists")     
+            return redirect(url_for("register.html")
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get())
+        }    
+
+
+      
 
 
 
