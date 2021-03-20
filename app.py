a36a7275755aa9057293ef0ab0b9cbc3b5b4592d
David-Gyavi <dbloosi12@gmail.com>
2021-03-20 01:43:53 +0000

    Add Logout functionality

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import (
-    Flask, render_template, 
+    Flask, flash,render_template, 
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
@@ -31,13 +31,13 @@ def get_fields():
 def register():
     if request.method == "POST":
         # Checking if username already exists in db
-        existing_users = mongo.db.users.find_one(
+        existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
 
 
-        if existing_users:
+        if existing_user:
             flash("Username already exists")     
-            return redirect(url_for("register.html"))
+            return redirect(url_for("register"))
 
         register = {
             "username": request.form.get("username").lower(),
@@ -51,7 +51,55 @@ def register():
     return render_template("register.html")
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # checking usernane if already exits in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                # invalid password match
+                 flash("Incorrect Username and/or Password")
+                 return redirect(url_for("login")) 
+
+        else:
+
+                # username doesn't exits
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login")) 
+
+    return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # grab the session user's username from db
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    # remove user from session cookies
+    flash("You were logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
+
+
+
 if __name__ == "__main__":
-    app.run(host=os.environ.get("IP" "0.0.0.0"),
-             port=int(os.environ.get("PORT", "5000")),
+    app.run(host=os.environ.get("IP"),
+             port=int(os.environ.get("PORT")),
              debug=True)    
