44704e4455e67d5c38b5eeba21dae1aa86c76038
David-Gyavi <dbloosi12@gmail.com>
2021-03-21 15:35:46 +0000

    Add contact form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.slider').slider();
+    $('.datepicker').datepicker({
+        format: "mmmm, dd, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
 });
