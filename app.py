360131e13b26724863a82d4baaae0a2d299d5658
David-Gyavi <dbloosi12@gmail.com>
2021-06-26 15:08:15 +0000

    about page deleted from the template

--- app.py
+++ app.py
@@ -119,7 +119,7 @@ def add_contact():
 
 
 
-if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
-            debug=os.environ.get("DEBUG"))
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)   
